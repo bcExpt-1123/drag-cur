2d10b5c84ad7bea71ab00d01823e5eefd7f3ca64
helper3876051 <daniel3876051@gmail.com>
2022-01-05 06:57:40 +0900

    complete

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+import DropFileInput from './components/drop-file-input/DropFileInput';
+
+function App() {
+
+    const onFileChange = (files) => {
+        console.log(files);
+    }
+
+    return (
+        <div className="box">
+            <DropFileInput
+                onFileChange={(files) => onFileChange(files)}
+            />
+        </div>
+    );
+}
+
+export default App;
